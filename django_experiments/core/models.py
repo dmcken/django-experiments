cbf0a49bb207268da03fd38c60a71d0b61b2c992
David Mc Ken <david.mcken@gmail.com>
2024-03-22 09:25:13 -0400

    Add core models.

--- django_experiments/core/models.py
+++ django_experiments/core/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Package(models.Model):
+    '''Package model.
+    '''
+    name = models.CharField(max_length=200)
+
+class Customer(models.Model):
+    '''Customer model.
+    '''
+    name = models.CharField(max_length=200)
+
+
+
+class Service(models.Model):
+    '''Service model.
+    '''
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    package  = models.ForeignKey(Package,  on_delete=models.CASCADE)
+    service_name = models.CharField(max_length=200)
